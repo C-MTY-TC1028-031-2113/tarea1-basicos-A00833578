8b5a5d541791e8738811989b0ec518a4bf279ae0
a00833578 <a00833578@itesm.mx>
2021-08-19 16:49:03 -0500

    My commit.

--- assignments/01Promedio/src/exercise.py
+++ assignments/01Promedio/src/exercise.py
@@ -1,7 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     pass
-
+    materia1 = float(input("Dame la calificacion de la primera materia: "))
+    materia2 = float(input("Dame la calificacion de la segunda materia: "))
+    materia3 = float(input("Dame la calificacion de la tercera materia: "))
+    materia4 = float(input("Dame la calificacion de la cuarta materia: "))
+    califtotal = (materia1 + materia2 + materia3 + materia4) / 4
+    print("Tu calificacion total es: " + str(califtotal))
 
 if __name__ == '__main__':
     main()
